f436f97b856a3d37a7385f5e5d54de85c9e1793f
Goga270 <gosha.aladin@gmail.com>
2022-06-06 19:29:07 +0300

    Update index.html

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -5,19 +5,37 @@
 //Menu SCRIPTS
 let menuImg = document.querySelector('.menu_img');
 let menuCloseImg = document.querySelector('.img_close_menu');
-let darkScene = document.querySelector('.brandDark');
+let darkScene = document.querySelectorAll('.brandDark');
 let menu = document.querySelector('.menu');
-let content = document.querySelector('.brand')
+let content = document.querySelector('.brand');
+let offers = document.querySelector('.offersWrap');
 menuImg.addEventListener('click', function (){
-    darkScene.classList.toggle('hidden');
+    for (let i=0;i<darkScene.length;i++){
+        darkScene[i].classList.toggle('hidden');
+    }
+    //darkScene.classList.toggle('hidden');
     menu.classList.toggle('hidden');
 });
 menuCloseImg.addEventListener('click', function (){
-    darkScene.classList.add('hidden');
+    for (let i=0;i<darkScene.length;i++){
+        darkScene[i].classList.add('hidden');
+    }
+    //darkScene.classList.add('hidden');
     menu.classList.add('hidden');
 });
 content.addEventListener('click', function (){
-    darkScene.classList.add('hidden');
+    for (let i=0;i<darkScene.length;i++){
+        darkScene[i].classList.add('hidden');
+    }
+    //darkScene.classList.add('hidden');
+    menu.classList.add('hidden');
+});
+
+offers.addEventListener('click', function (){
+    for (let i=0;i<darkScene.length;i++){
+        darkScene[i].classList.add('hidden');
+    }
+    //darkScene.classList.add('hidden');
     menu.classList.add('hidden');
 });
 
